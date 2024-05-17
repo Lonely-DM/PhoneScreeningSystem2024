e640ac22cf46052c95fcfe68eb8fe51f0063fd83
Thimaya Dassanayake <140379796+Thimaya03@users.noreply.github.com>
2024-05-17 19:02:48 +1000

    Add files via upload

---
+++ Frontend Files/LoginPage - Rohani/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import LoginSignin from './Components/LoginSignin/LoginSignin';
+
+function App() {
+  return (
+    <div>
+      <LoginSignin/>
+      
+
+   </div>
+  );
+}
+
+export default App;
+
+
+
+
+
+
+
