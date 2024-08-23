19eb82835ba8374f9578a61da35aceea4557c5e0
Shujian.Lin <2669839973@qq.com>
2024-08-23 12:22:47 +1000

    Add TicketDetail Route and page

---
+++ React Web App - Muhammed + Rohani/src/Components/TicketDetail/mockData.js
@@ -0,0 +1,15 @@
+const mockData = {
+    ticketId: 'SRV.89871921',
+    status: 'Resolved', // Options: 'Resolved', 'In-Progress', 'Received'
+    client: {
+        name: 'kaylasmith1938',
+        contact: 'kaylasmith1938@xxx.com'
+    },
+    inquiryTag: 'General Enquiry',
+    priority: 'HIGH', // Options: 'HIGH', 'MEDIUM', 'LOW'
+    description: 'Sample description text',
+    assignedOfficer: 'James Edwards',
+    latestUpdate: '29/7/2024 Created'
+};
+
+export default mockData;
