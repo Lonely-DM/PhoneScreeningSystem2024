d68719c9a8dbb8a9452fd34d3e841c34fbf8506d
zvnga <110079073+zvnga@users.noreply.github.com>
2024-09-23 11:58:58 +1000

    Add files via upload

---
+++ Frontend Files/Ticket History Page - Zunga/scripts.js
@@ -0,0 +1,22 @@
+// Potential Ticket History JS for the SRV Ticketing Solution by Zunga Siakalima
+
+// Sidebar Toggle Function
+var sidebarOpen = false;
+var sidebar = document.getElementById("sidebar");
+
+function openSidebar () {
+    if(!sidebarOpen){
+        sidebar.classList.add("sidebar-responsive");
+        sidebarOpen = true;
+    }
+}
+
+function closeSidebar () {
+    if(!sidebarOpen){
+        sidebar.classList.add("sidebar-responsive");
+        sidebarOpen = false;
+    }
+}
+
+//JS Table Script
+new DataTable('#example');
